668aaaf8ff92db86a5817cd841af56237e4b3604
cooper09 <cooper@sonyainc.net>
2019-04-11 12:38:28 -0400

    ready to add my ad

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, Image } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,12 +16,11 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to My Never-Ending Misery!</Text>
+        <Text style={styles.welcome}>Welcome to My  Context Ad</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
@@ -47,3 +46,4 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
